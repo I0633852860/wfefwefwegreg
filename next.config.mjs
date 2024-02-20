22211bd3a87722618090c266a76776db766b1c95
I0633852860 <i0633852860@gmail.com>
2024-02-20 22:00:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.mjs
@@ -0,0 +1,66 @@
+const BuildOptions = {
+  standalone: !!process.env.BUILD_STANDALONE,
+};
+
+/** @type {import('next').NextConfig} */
+let nextConfig = {
+  reactStrictMode: true,
+
+  // [exporting] https://nextjs.org/docs/advanced-features/static-html-export
+  ...BuildOptions.standalone && {
+    // Export the frontend to ./dist
+    output: 'standalone',
+    distDir: 'dist',
+
+    // Disable Image optimization
+    images: { unoptimized: true },
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+  },
+
+  // [puppeteer] https://github.com/puppeteer/puppeteer/issues/11052
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core'],
+  },
+
+  webpack: (config, _options) => {
+    // @mui/joy: anything material gets redirected to Joy
+    config.resolve.alias['@mui/material'] = '@mui/joy';
+
+    // @dqbd/tiktoken: enable asynchronous WebAssembly
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    // [exporting] prevent too many small files (50kb)
+    BuildOptions.standalone && (config.optimization.splitChunks.minSize = 50 * 1024);
+
+    return config;
+  },
+
+  // Note: disabled to check whether the project becomes slower with this
+  // modularizeImports: {
+  //   '@mui/icons-material': {
+  //     transform: '@mui/icons-material/{{member}}',
+  //   },
+  // },
+
+  // Uncomment the following leave console messages in production
+  // compiler: {
+  //   removeConsole: false,
+  // },
+};
+
+// Validate environment variables, if set at build time. Will be actually read and used at runtime.
+// This is the reason both this file and the servr/env.mjs files have this extension.
+await import('./src/server/env.mjs');
+
+// conditionally enable the nextjs bundle analyzer
+if (process.env.ANALYZE_BUNDLE) {
+  const { default: withBundleAnalyzer } = await import('@next/bundle-analyzer');
+  nextConfig = withBundleAnalyzer({ openAnalyzer: true })(nextConfig);
+}
+
+export default nextConfig;
